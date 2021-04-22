605be97f534638468482c6d8642b505b6e45b6c7
Omar Lopez <darkoware@gmail.com>
2021-04-21 19:34:57 -0500

    Agregada validacion para que no se inicie una declaracion de conclusion si no se ha hecho una inicial o de modificacion

--- src/app/presentar-declaracion/comienza-tu-declaracion/comienza-tu-declaracion.component.ts
+++ src/app/presentar-declaracion/comienza-tu-declaracion/comienza-tu-declaracion.component.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { Apollo } from 'apollo-angular';
 import { declaracionesMetadata } from '@api/declaracion';
 import { DialogComponent } from '@app/@shared';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-comienza-tu-declaracion',
@@ -20,40 +21,56 @@ export class ComienzaTuDeclaracionComponent implements OnInit {
   }
 
   //OMAR: Validar que no se pueda iniciar una declaracion simplificada o completa si ya se tiene una que no se ha terminado
-  iniciarDeclaracion(tipo: String, modo: String) {
+  async iniciarDeclaracion(tipo: String, modo: String) {
     //obtener la ultima declaracion
+    if (tipo == 'CONCLUSION' && !(await this.validarDeclaracionDeConclusion())) {
+      return;
+    }
+
     this.validarDeclaracionIniciada(tipo, modo);
+  }
 
-    // this.apollo.query({
-    //   query: declaracionesMetadata,
-    //   variables: {
-    //     filter: {
-    //       firmada: false,
-    //       tipoDeclaracion: tipo
-    //     },
-    //   },
+  async validarDeclaracionDeConclusion() {
+    const tieneIncial = await this.tieneDeclaracionFinalizada('INICIAL');
+    const tieneModificacion = await this.tieneDeclaracionFinalizada('MODIFICACION');
 
-    // })
-    // .toPromise()
-    // .then((result: any)=> {
-    //   if(result.data.declaracionesMetadata.docs.length > 0) {
-    //     // tiene una declaracion abierta, debe cerrarla para poder comenzar otra
-    //     this.dialog.open(DialogComponent, {
-    //       data: {
-    //         title: 'Error',
-    //         message: 'Ya tiene una declaración iniciada, debe finalizarla para poder iniciar otra',
-    //         trueText: 'Aceptar',
-    //       },
-    //     });
-    //   }
-    //   else {
-    //     if(modo === 'simplificada')
-    //       this.router.navigate(['/inicial/simplificada/situacion-patrimonial']);
-    //     else
-    //       this.router.navigate(['/inicial/situacion-patrimonial']);
+    if (!tieneIncial && !tieneModificacion) {
+      this.dialog.open(DialogComponent, {
+        data: {
+          title: 'Error',
+          message:
+            'Para poder realizar una declaración de conclusión, es necesario tener una declaración inicial o de modificación concluida',
+          trueText: 'Aceptar',
+        },
+      });
 
-    //   }
-    // });
+      return false;
+    }
+
+    return true;
+  }
+
+  async tieneDeclaracionFinalizada(tipo: String) {
+    // si van a hacer una de conclusión, debe tener una inicial o de modificación terminada
+    return await this.apollo
+      .query({
+        query: declaracionesMetadata,
+        variables: {
+          filter: {
+            firmada: true,
+            tipoDeclaracion: tipo,
+          },
+        },
+      })
+      .toPromise()
+      .then((data: any) => {
+        if (data.data.declaracionesMetadata.docs.length > 0) {
+          // tiene una declaracion finalizada, puede hacer la de conclusion
+          return true;
+        } else {
+          return false;
+        }
+      });
   }
 
   async validarDeclaracionIniciada(tipo: String, modo: String) {
@@ -82,7 +99,7 @@ export class ComienzaTuDeclaracionComponent implements OnInit {
       } else {
         if (modo === 'simplificada')
           this.router.navigate(['/' + tipo.toLowerCase() + '/simplificada/situacion-patrimonial']);
-        else this.router.navigate(['/inicial/situacion-patrimonial']);
+        else this.router.navigate(['/' + tipo.toLowerCase() + '/situacion-patrimonial']);
       }
     } catch (error) {
       console.log(error);
